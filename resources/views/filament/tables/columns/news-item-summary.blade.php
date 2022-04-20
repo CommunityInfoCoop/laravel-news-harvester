3321867545875587b798ac798a13bad0286a52cb
Chris Hardie <chris@chrishardie.com>
2022-04-20 13:20:25 -0400

    Adjust display of meta, display full content for social posts

--- resources/views/filament/tables/columns/news-item-summary.blade.php
+++ resources/views/filament/tables/columns/news-item-summary.blade.php
@@ -3,12 +3,17 @@
         $record = $getRecord();
     @endphp
     <div class="grow flex flex-col">
-        <div class="news-item-meta text-sm text-gray-600 dark:text-gray-300"><span class="news-item-publish-timestamp">{{ $record->publish_timestamp->timezone(config('news-harvester.display_time_zone'))->format('M d, g:i a') }}</span>
-            / <span class="news-item-source-name">{{ $record->source_info->name }}</span>
-            / <span class="news-item-feed-name">{{ $record->feed->name }}</span>
+        <div class="news-item-meta text-sm text-gray-600 dark:text-gray-300">
+            <span class="news-item-source-name text-base block">{{ $record->source_info->name }}</span>
+            <span class="news-item-publish-timestamp block">{{ $record->publish_timestamp->timezone(config('news-harvester.display_time_zone'))->format('M d, g:i a') }}</span>
         </div>
-        <div class="news-item-title font-bold hover:underline focus:outline-none focus:underline filament-tables-link text-black-600 hover:text-black-500 dark:text-primary-500 dark:hover:text-primary-400">{{ $record->title }}</div>
-        <div class="news-item-excerpt text-sm text-gray-600 dark:text-gray-300 pr-8 hidden sm:flex">{{ $record->excerpt }}</div>
+        @if(! $record->isSocial)
+            <div class="news-item-title font-bold hover:underline focus:outline-none focus:underline filament-tables-link text-black-600 hover:text-black-500 dark:text-primary-500 dark:hover:text-primary-400">{{ $record->title }}</div>
+            <div class="news-item-excerpt text-sm text-gray-600 dark:text-gray-300 pr-8 hidden sm:flex">{{ $record->excerpt }}</div>
+        @else
+            <div class="news-item-excerpt text-sm text-gray-600 dark:text-gray-300 pr-8 hidden sm:flex">{{ $record->content }}</div>
+        @endif
+
     </div>
     @if($record->media_url)
         <div class="hidden sm:flex align-self-end items-center justify-center w-24 max-h-16 overflow-hidden">
