ea7a9ffbe4e5de6b7985166b1cf6517c09e20a5b
Chris Hardie <chris@chrishardie.com>
2022-04-21 12:55:29 -0400

    News item images: center above stories/content and make larger

--- resources/views/filament/tables/columns/news-item-summary.blade.php
+++ resources/views/filament/tables/columns/news-item-summary.blade.php
@@ -7,6 +7,11 @@
             <span class="news-item-source-name text-base block">{{ $record->source_info->name }}</span>
             <span class="news-item-publish-timestamp block">{{ $record->publish_timestamp->timezone(config('news-harvester.display_time_zone'))->format('M d, g:i a') }}</span>
         </div>
+        @if($record->media_url)
+            <div class="hidden sm:flex items-center justify-center overflow-hidden py-2">
+                <img class="self-center" src="{{ $record->media_url }}" alt="Media image for {{ $record->title }}" />
+            </div>
+        @endif
         @if(! $record->isSocial)
             <div class="news-item-title font-bold hover:underline focus:outline-none focus:underline filament-tables-link text-black-600 hover:text-black-500 dark:text-primary-500 dark:hover:text-primary-400">{{ $record->title }}</div>
             <div class="news-item-excerpt text-sm text-gray-600 dark:text-gray-300 pr-8 hidden sm:flex">{{ $record->excerpt }}</div>
@@ -15,9 +20,4 @@
         @endif
 
     </div>
-    @if($record->media_url)
-        <div class="hidden sm:flex align-self-end items-center justify-center w-24 max-h-16 overflow-hidden">
-            <img class="self-center" src="{{ $record->media_url }}" alt="Media image for {{ $record->title }}" />
-        </div>
-    @endif
 </div>
